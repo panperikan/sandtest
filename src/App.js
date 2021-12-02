4e08164348eb469088dda9a40ee8801c27b1240b
panperikan <78346124+panperikan@users.noreply.github.com>
2021-12-02 10:41:25 +0900

    add1041

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>そんな</p>
+      <span>処理</span>
     </div>
   );
 }
